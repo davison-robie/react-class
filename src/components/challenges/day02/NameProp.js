20f6825a7dbf2eed315bcc71b97374ce5838550a
Davison Robie <AHDR@davisonsmacbook.attlocal.net>
2021-02-04 18:00:07 -0500

    bootstrap added

---
+++ src/components/challenges/day02/NameProp.js
@@ -0,0 +1,12 @@
+// Props, short for Properties
+//Parameters of a component function that get passed into the component to aid in its reusability
+//Dynamic data can be assigned to child components
+//
+
+//import React from "react";
+
+const NameProp = (props) => { // property returning React element
+    return <h1>Hello, { props.name }</h1>
+};
+
+export default NameProp;
---
+++ src/components/challenges/day02/State.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const State = () => {
+    const [count, setCount] = useState();
+    function decCount() {
+        setCount(count - 1);
+    }
+    function incCount() {
+        setCount(count + 1);
+    }
+    return (
+        <>
+        <button onClick={decCount}>-</button>
+        <span>  { count }  </span>
+        <button onClick={incCount}>+</button>
+        </>
+    )
+}
+
+export default State;
